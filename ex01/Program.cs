4053706e966c399b12c88fd2cea0e44f961c825b
Caio Santos <caiosilvasantos87@gmail.com>
2024-06-15 23:11:30 -0300

    15/06 - exs -classes, objetos e atributos

---
+++ ex01/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex01
+{
+    internal class Program
+    {
+
+        // Fazer um programa para ler os dados de duas pessoas, depois mostrar o nome da pessoa mais velha
+
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1, pessoa2;
+            pessoa1 = new Pessoa();
+            pessoa2 = new Pessoa();
+
+            Console.WriteLine("Dados da primeira pessoa:");
+            Console.Write("Nome: ");
+            pessoa1.NOME = Console.ReadLine();
+            Console.Write("Idade: ");
+            pessoa1.IDADE =int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa:");
+            Console.Write("Nome: ");
+            pessoa2.NOME = Console.ReadLine();
+            Console.Write("Idade: ");
+            pessoa2.IDADE = int.Parse(Console.ReadLine());
+
+            if (pessoa1.IDADE > pessoa2.IDADE)
+            {
+                Console.WriteLine($"Pessoa mais velha: {pessoa1.NOME}");
+            }
+            else
+            {
+                Console.WriteLine($"Pessoa mais velha: {pessoa2.NOME}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Pessoa
+    {
+        public string NOME;
+        public int IDADE;
+    }
+}
---
+++ ex02/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace ex02
+{
+    // Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário médio dos funcionários.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionarios funcionario1, funcionario2;
+            funcionario1 = new Funcionarios();
+            funcionario2 = new Funcionarios();
+
+            Console.WriteLine("Dados do primeiro funcionario: ");
+            Console.Write("Nome: ");
+            funcionario1.NOME = Console.ReadLine();
+            Console.Write("Salario: ");
+            funcionario1.SALARIO = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do segundo funcionario: ");
+            Console.Write("Nome: ");
+            funcionario2.NOME = Console.ReadLine();
+            Console.Write("Salario: ");
+            funcionario2.SALARIO = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media, soma; 
+            soma = funcionario2.SALARIO + funcionario1.SALARIO;
+            media = soma / 2.0;
+
+            Console.WriteLine($"O salario medio desses funcionarios é: {media.ToString("f2", CultureInfo.InvariantCulture)}");
+            Console.ReadLine();
+        }
+    }
+
+    class Funcionarios
+    {
+        public string NOME;
+        public double SALARIO;
+    }
+}
